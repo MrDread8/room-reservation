980925cd526d385c623398fd6bb93c8d002f91fa
riviofficial <riviofficial.kontakt@gmail.com>
2019-12-08 21:42:55 +0100

    In Progress: Login Form

---
+++ index.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/index.css">
+    <title>Room Reservation</title>
+</head>
+<body>
+    <div id="content">
+        <form action="components/login.php" method="POST" enctype="multipart/form-data">
+            <input type="text" name="password" placeholder="login">
+            <input type="password" name="password" placeholder="password">
+            <input type="button" value="LOG IN">
+        </form>
+    </div>
+</body>
+</html>
